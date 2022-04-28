5203585ee86cadbded8207e607b6ade80e563dae
prakharparwal <prakhar.parwal@thoughtworks.com>
2022-04-28 18:43:15 +0530

    Initial commit

---
+++ src/main/java/com/versed/app/VersedAppApplication.java
@@ -0,0 +1,13 @@
+package com.versed.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersedAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersedAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/versed/app/VersedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.versed.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
